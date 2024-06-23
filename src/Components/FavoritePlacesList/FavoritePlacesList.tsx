b12600014087e9f1cd8def0bccbe582de8286fe9
HegryLuis <hegry.luis@gmail.com>
2024-06-23 07:41:42 +0300

    add many functionals

--- src/Components/FavoritePlacesList/FavoritePlacesList.tsx
+++ src/Components/FavoritePlacesList/FavoritePlacesList.tsx
@@ -67,10 +67,17 @@ const FavoritePlacesList: React.FC = () => {
         id: placeData.result.place_id,
         name: placeData.result.name,
         vicinity: placeData.result.vicinity,
+        opening_hours: {
+          isOpen:
+            placeData.result.opening_hours &&
+            placeData.result.opening_hours.open_now !== undefined
+              ? placeData.result.opening_hours.open_now
+              : null,
+          weekday_text: placeData.result.opening_hours?.weekday_text,
+        },
         rating: placeData.result.rating,
-        opening_hours: placeData.result.opening_hours,
         type: placeData.type,
-        geometry: placeData.geometry,
+        geometry: placeData.result.geometry,
       })
     );
 
@@ -78,14 +85,35 @@ const FavoritePlacesList: React.FC = () => {
   }, [detailedPlacesData]);
 
   const handleShowInMap = (place: Place) => {
-    if (map) {
-      clearMarkers(markers, setMarkers);
-      const position = new google.maps.LatLng(
-        place.geometry.location.lat(),
-        place.geometry.location.lng()
-      );
-      addMarker(position, map, markers, setMarkers);
-      map.panTo(position);
+    console.log(place);
+    if (place.geometry && place.geometry.location) {
+      if (map) {
+        clearMarkers(markers, setMarkers);
+        const position = {
+          lat: place.geometry.location.lat,
+          lng: place.geometry.location.lng,
+        } as google.maps.LatLng;
+        addMarker(position, map, markers, setMarkers);
+        map.panTo(position);
+      }
+    } else {
+      console.error("Invalid place object:", place);
+    }
+  };
+
+  const handleDelete = async (id: string) => {
+    if (user) {
+      try {
+        await axios.delete(
+          `http://localhost:3001/users/${user.id}/favourite-places/${id}`
+        );
+
+        setDetailedPlaces((prevPlaces) =>
+          prevPlaces.filter((place) => place.id !== id)
+        );
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -97,23 +125,71 @@ const FavoritePlacesList: React.FC = () => {
           <div className="text-info">
             {detailedPlaces.map((place: Place) => (
               <li key={place.id}>
-                <h2>{place.name}</h2>
-                <p className="address">Address: {place.vicinity}</p>
-                {place.rating && (
-                  <p className={`rating ${getRatingClass(place.rating)}`}>
-                    Rating: {place.rating}
-                  </p>
-                )}
-                {place.opening_hours && (
-                  <p className="opening-hours">
-                    Open now:{" "}
-                    {place.opening_hours.isOpen
-                      ? place.opening_hours.isOpen()
-                        ? "Yes"
-                        : "No"
-                      : "Not indicated"}
-                  </p>
-                )}
+                <div className="place-block">
+                  <h2>{place.name}</h2>
+                  <p className="address">Address: {place.vicinity}</p>
+                  {place.rating && (
+                    <p className={`rating ${getRatingClass(place.rating)}`}>
+                      Rating: {place.rating}
+                    </p>
+                  )}
+                  {place.opening_hours?.weekday_text && (
+                    <div className="weekday-text">
+                      <p>Weekday hours:</p>
+                      <p className="weekday">
+                        {place.opening_hours.weekday_text.map(
+                          (weekday: string, index: number) => (
+                            <span key={index}>{weekday}</span>
+                          )
+                        )}
+                      </p>
+                    </div>
+                  )}
+                </div>
+                <div className="place-btn">
+                  <button
+                    className="delete-btn"
+                    onClick={() => handleDelete(place.id)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      viewBox="0 0 490.646 490.646"
+                    >
+                      <g>
+                        <g>
+                          <path
+                            fill="#fff"
+                            d="M399.179 67.285l-74.794 0.033L324.356 0 166.214 0.066l0.029 67.318-74.802 0.033 0.025 62.914h307.739z M198.28 32.11l94.03-0.041 0.017 35.262-94.03 0.041z"
+                          />
+                          <path
+                            fill="#fff"
+                            d="M91.465 490.646h307.739V146.359H91.465V490.646z M317.461 193.372h16.028v250.259h-16.028V193.372z M237.321 193.372h16.028v250.259h-16.028V193.372z M157.18 193.372h16.028v250.259H157.18V193.372z"
+                          />
+                        </g>
+                      </g>
+                    </svg>
+                  </button>
+                  <button
+                    className="show-btn"
+                    onClick={() => handleShowInMap(place)}
+                  >
+                    <svg
+                      width="25"
+                      height="25"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="#ffffff"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M11.291 21.706 12 21l-.709.706zM12 21l.708.706a1 1 0 0 1-1.417 0l-.006-.007-.017-.017-.062-.063a47.708 47.708 0 0 1-1.04-1.106 49.562 49.562 0 0 1-2.456-2.908c-.892-1.15-1.804-2.45-2.497-3.734C4.535 12.612 4 11.248 4 10c0-4.539 3.592-8 8-8 4.408 0 8 3.461 8 8 0 1.248-.535 2.612-1.213 3.87-.693 1.286-1.604 2.585-2.497 3.735a49.583 49.583 0 0 1-3.496 4.014l-.062.063-.017.017-.006.006L12 21zm0-8a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </li>
             ))}
           </div>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import axios from "axios";
 import { useApp } from "context";
@@ -31,19 +31,45 @@ const logIn = async (name: string, password: string) => {
   }
 };
 
+const checkPassword = async (userId: string, password: string) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3001/users/check-password",
+      {
+        userId,
+        password,
+      }
+    );
+    console.log("Password verified");
+    return true;
+  } catch (error: any) {
+    console.error("Error checking password:", error.response.data);
+    throw error;
+  }
+};
+
 const Header: React.FC = () => {
   const [modalActive, setModalActive] = useState(false);
+  const [isChangeModalActive, setIsChangeModalActive] = useState(false);
   const [isSighUp, setIsSighUp] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { isLoggedIn, setIsLoggedIn, user, setUser } = useApp();
+  const [newUsername, setNewUsername] = useState(user ? user.name : "");
+  const [newPassword, setNewPassword] = useState("");
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [currentPasswordValid, setCurrentPasswordValid] = useState(false);
+
+  useEffect(() => {
+    setCurrentPasswordValid(false);
+  }, [isChangeModalActive]);
 
   const handleSighUp = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
       const newUser = await signUp(username, password);
-      console.log("NEw = ", newUser);
+      console.log("New user =", newUser);
       setUser(newUser);
       setIsLoggedIn(true);
       setModalActive(false);
@@ -57,8 +83,8 @@ const Header: React.FC = () => {
 
     try {
       const loggedInUser = await logIn(username, password);
-      console.log("loggedInUser = ", loggedInUser);
       setUser(loggedInUser);
+      setNewUsername(loggedInUser.name);
       setIsLoggedIn(true);
       setModalActive(false);
     } catch (error: any) {
@@ -66,8 +92,66 @@ const Header: React.FC = () => {
     }
   };
 
+  const handleChangeLoginPassword = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+    try {
+      // Проверить текущий пароль перед обновлением
+      const passwordVerified = await checkPassword(
+        user?.id || "",
+        currentPassword
+      );
+      if (!passwordVerified) {
+        throw new Error("Current password does not match.");
+      }
+
+      const response = await axios.put(
+        `http://localhost:3001/users/${user?.id}`,
+        {
+          name: newUsername,
+          password: newPassword,
+        }
+      );
+
+      setNewPassword("");
+      setCurrentPassword("");
+      setIsChangeModalActive(false);
+    } catch (error) {
+      console.error("Error updating login/password ", error);
+    }
+  };
+
+  const handleLogOut = () => {
+    setUser(null);
+    setIsLoggedIn(false);
+    setModalActive(false); // Close modal if open
+  };
+
   const toggleModal = () => {
     setModalActive(!modalActive);
+  };
+
+  const toggleChangeModal = () => {
+    setIsChangeModalActive(!isChangeModalActive);
+  };
+
+  // Обработчик изменения текущего пароля
+  const handleCurrentPasswordChange = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = e.target.value;
+    setCurrentPassword(value); // Обновляем текущее значение поля
+
+    try {
+      // Проверяем текущий пароль динамически при каждом изменении
+      const passwordVerified = await checkPassword(user?.id || "", value);
+      setCurrentPasswordValid(passwordVerified); // Обновляем состояние валидации
+    } catch (error) {
+      console.error("Error checking current password:", error);
+      setCurrentPasswordValid(false); // Если ошибка, считаем пароль неверным
+    }
   };
 
   return (
@@ -87,12 +171,26 @@ const Header: React.FC = () => {
             />
           </svg>
         </div>
-        <div className="header-title">Explore Nearby</div>
+        <div className="header-title animated-text">
+          <span>Explore Nearby</span>
+        </div>
       </div>
 
       <div className="header-right">
         {isLoggedIn ? (
-          <div className="greeting">Hello, {user?.name}!</div>
+          <div className="greeting">
+            <p>
+              Hello, <span>{newUsername}!</span>
+            </p>
+            <div className="greeting-buttons">
+              <button className="btn" onClick={handleLogOut}>
+                Log Out
+              </button>
+              <button className="btn" onClick={toggleChangeModal}>
+                Change login/password
+              </button>
+            </div>
+          </div>
         ) : (
           <button className="header-button" onClick={toggleModal}>
             Sign up/Log in
@@ -113,7 +211,6 @@ const Header: React.FC = () => {
               }}
               className={!isSighUp ? "active" : ""}
             >
-              {" "}
               Log in
             </span>
             <span
@@ -122,7 +219,7 @@ const Header: React.FC = () => {
               }}
               className={isSighUp ? "active" : ""}
             >
-              Sigh up
+              Sign up
             </span>
           </h6>
 
@@ -140,7 +237,7 @@ const Header: React.FC = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <button>Sigh up</button>
+              <button>Sign up</button>
             </form>
           ) : (
             <form className="login" onSubmit={handleLogIn}>
@@ -159,6 +256,42 @@ const Header: React.FC = () => {
               <button>Log in</button>
             </form>
           )}
+        </div>
+      </div>
+
+      <div className={`modal ${isChangeModalActive ? "active" : ""}`}>
+        <div className="modal-content">
+          <button className="close-button" onClick={toggleChangeModal}>
+            <span>X</span>
+          </button>
+
+          <h6>Change login/password</h6>
+
+          <form className="login" onSubmit={handleChangeLoginPassword}>
+            <input
+              type="text"
+              placeholder="New username"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="New password"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Current password"
+              value={currentPassword}
+              onChange={handleCurrentPasswordChange} // Обработчик для динамической проверки текущего пароля
+            />
+            {currentPasswordValid ? (
+              <button>Update</button>
+            ) : (
+              <p className="error-message">Current password is incorrect.</p>
+            )}
+          </form>
         </div>
       </div>
     </div>
